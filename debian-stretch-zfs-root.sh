4ce3ca429a74781799ffd3044e5b37aa35b7a525
Alexander Efimov <alexandr.efimov@gmail.com>
2018-02-16 16:43:34 +0500

    fix for VIRTIO disk in KVM

--- debian-stretch-zfs-root.sh
+++ debian-stretch-zfs-root.sh
@@ -47,7 +47,7 @@ while read -r IDLINK; do
 	BYID["$(basename "$(readlink "$IDLINK")")"]="$IDLINK"
 done < <(find /dev/disk/by-id/ -type l)
 
-for DISK in $(lsblk -I8 -dn -o name); do
+for DISK in $(lsblk -I8 -I254 -dn -o name); do
 	if [ -z "${BYID[$DISK]}" ]; then
 		SELECT+=("$DISK" "(no /dev/disk/by-id persistent device name available)" off)
 	else
